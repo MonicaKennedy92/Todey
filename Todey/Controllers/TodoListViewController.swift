8b507a92613ffe2257537aa8538ed858a9e77d32
Monica <monica.k@spi-global.com>
2019-03-25 11:33:50 +0530

    Core Data Init

---
+++ Todey/Controllers/TodoListViewController.swift
@@ -0,0 +1,119 @@
+//
+//  TodoListViewController.swift
+//  Todey
+//
+//  Created by lw-dlf on 3/21/19.
+//  Copyright © 2019 lw-dlf. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+   var defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        print(dataFilePath)
+        
+        let newItem = Item()
+        newItem.title = "One"
+        itemArray.append(newItem)
+        
+        
+        let newItem1 = Item()
+        newItem1.title = "Two"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Three"
+        itemArray.append(newItem2)
+        
+        loadItems()
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
+    }
+
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+
+        return cell
+    }
+    
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    //MArk - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+//            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.saveItems()
+           
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+       present(alert, animated: true, completion: nil)
+        
+    }
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)//write(to:self.dataFilePath!)
+        } catch {
+            print(error)
+        }
+        
+        self.tableView.reloadData()
+
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
---
+++ Todey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todey
+//
+//  Created by lw-dlf on 3/21/19.
+//  Copyright © 2019 lw-dlf. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable{
+    var title: String = ""
+    var done: Bool = false
+}
